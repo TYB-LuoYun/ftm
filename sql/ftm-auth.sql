62393407d11d7fd16e5df7d3e7983185bd95e367
gva-icp <1455835671@qq.com>
2022-05-12 17:13:09 +0800

    sql

---
+++ sql/ftm-auth.sql
@@ -0,0 +1,100 @@
+/*
+SQLyog Ultimate v8.32 
+MySQL - 5.7.30 : Database - ftm-auth
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ftm-auth` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `ftm-auth`;
+
+/*Table structure for table `oauth_access_token` */
+
+DROP TABLE IF EXISTS `oauth_access_token`;
+
+CREATE TABLE `oauth_access_token` (
+  `token_id` varchar(128) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(128) DEFAULT NULL,
+  `client_id` varchar(128) DEFAULT NULL,
+  `authentication` blob,
+  `refresh_token` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `oauth_access_token` */
+
+/*Table structure for table `oauth_client_details` */
+
+DROP TABLE IF EXISTS `oauth_client_details`;
+
+CREATE TABLE `oauth_client_details` (
+  `client_id` varchar(128) NOT NULL,
+  `resource_ids` varchar(128) DEFAULT NULL,
+  `client_secret` varchar(128) DEFAULT NULL,
+  `scope` varchar(128) DEFAULT NULL,
+  `authorized_grant_types` varchar(128) DEFAULT NULL,
+  `web_server_redirect_uri` varchar(128) DEFAULT NULL,
+  `authorities` varchar(128) DEFAULT NULL,
+  `access_token_validity` int(11) DEFAULT NULL,
+  `refresh_token_validity` int(11) DEFAULT NULL,
+  `additional_information` varchar(4096) DEFAULT NULL,
+  `autoapprove` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`client_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `oauth_client_details` */
+
+insert  into `oauth_client_details`(`client_id`,`resource_ids`,`client_secret`,`scope`,`authorized_grant_types`,`web_server_redirect_uri`,`authorities`,`access_token_validity`,`refresh_token_validity`,`additional_information`,`autoapprove`) values ('mxg-blog-admin','','$2a$10$uA51hWL5yusFBoEvZOAZbeaYYqUaFV7xjdDB8GA.4iViNiCSK9xKO','all','password,refresh_token,authorization_code','http://localhost:7001/login','all',NULL,NULL,NULL,'false');
+
+/*Table structure for table `oauth_client_token` */
+
+DROP TABLE IF EXISTS `oauth_client_token`;
+
+CREATE TABLE `oauth_client_token` (
+  `token_id` varchar(128) DEFAULT NULL,
+  `token` blob,
+  `authentication_id` varchar(128) NOT NULL,
+  `user_name` varchar(128) DEFAULT NULL,
+  `client_id` varchar(128) DEFAULT NULL,
+  PRIMARY KEY (`authentication_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `oauth_client_token` */
+
+/*Table structure for table `oauth_code` */
+
+DROP TABLE IF EXISTS `oauth_code`;
+
+CREATE TABLE `oauth_code` (
+  `code` varchar(128) DEFAULT NULL,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `oauth_code` */
+
+/*Table structure for table `oauth_refresh_token` */
+
+DROP TABLE IF EXISTS `oauth_refresh_token`;
+
+CREATE TABLE `oauth_refresh_token` (
+  `token_id` varchar(128) DEFAULT NULL,
+  `token` blob,
+  `authentication` blob
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `oauth_refresh_token` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
