fb2205a28f5d42c4a084e400ec2c748fbcef4999
TYB-LuoYun <92981594+TYB-LuoYun@users.noreply.github.com>
2022-06-24 14:04:06 +0800

    fix gitignore

--- ftm-gateway/src/main/java/top/anets/GatewayServerApplication.java
+++ ftm-gateway/src/main/java/top/anets/GatewayServerApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient // nacos客户端
 @SpringBootApplication
 public class GatewayServerApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(GatewayServerApplication.class, args);
     }
